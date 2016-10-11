e0e55cf04fd8156b5dc59be7c60ee9d0eda8192e
Jakob Egger <jakob@eggerapps.at>
2016-10-11 17:28:58 +0200

    Show Photo Copyrights

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,15 @@
 class Photo < ActiveRecord::Base
+  belongs_to :copyright, optional: true
+  belongs_to :author, optional: true
+  
+  def caption
+    caption = ''
+    if self.author
+      caption += self.author.first_name + ' ' + self.author.last_name + ' '
+    end
+    caption += self.copyright.try(:copyright) || ''
+    caption += ' '
+    caption += self.copyright_detail || ''
+    caption
+  end
 end
