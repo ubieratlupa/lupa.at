5d75993e4e3be487886163ac88b1ff0c1883f58c
Jakob Egger <jakob@eggerapps.at>
2016-10-11 09:52:09 +0200

    Set page title to monument title

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -3,6 +3,7 @@ class MonumentsController < ApplicationController
   def show
     @monument = Monument.find(params[:id])
     @photos = @monument.photos.order(:ord)
+    @title = @monument.title
   end
 
 end
