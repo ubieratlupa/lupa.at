810629dbad271e8dc2aa7b585a519b21a9cc558b
Jakob Egger <jakob@eggerapps.at>
2017-03-08 10:45:10 +0100

    Change page model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,6 @@ class PagesController < ApplicationController
   def show
     @other_pages = Page.where('category = ?', 'about').order(:ord)
     @current_page = Page.find(params[:id])
-    @title = @current_page.title
-    @text = @current_page.text
-    @text.gsub! "NUMBER_OF_PHOTOS", Photo.all.size.to_s
-    @text.gsub! "NUMBER_OF_MONUMENTS", Monument.all.size.to_s
   end
 
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,18 @@
 class Page < ActiveRecord::Base
   belongs_to :photo, optional: true
+  
+  def display_text
+    display_text = text
+    display_text.gsub! "NUMBER_OF_PHOTOS", Photo.all.size.to_s if display_text.match("NUMBER_OF_PHOTOS")
+    display_text.gsub! "NUMBER_OF_MONUMENTS", Monument.all.size.to_s if display_text.match("NUMBER_OF_MONUMENTS")
+    display_text
+  end
+  
+  def display_title
+    display_title = title
+    display_title.gsub! "NUMBER_OF_PHOTOS", Photo.all.size.to_s if display_title.match("NUMBER_OF_PHOTOS")
+    display_title.gsub! "NUMBER_OF_MONUMENTS", Monument.all.size.to_s if display_title.match("NUMBER_OF_MONUMENTS")
+    display_title
+  end
+  
 end
