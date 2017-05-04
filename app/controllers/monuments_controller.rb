5059f4f58a3ff1532415a959d83437de22452ad0
Jakob Egger <jakob@eggerapps.at>
2017-05-04 09:11:45 +0200

    Neuzugänge nach Bildern

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -14,15 +14,15 @@ class MonumentsController < ApplicationController
 
   def index
     @query = Query.find(1)
-    @new_monuments = Monument.where("date_trunc('month',created) = (select date_trunc('month',max(created)) from monuments where visible)")
-    @new_monuments = @new_monuments.order("exists (select * from photos where monument_id = monuments.id) desc, id desc")
+    @new_monuments = Monument.where("id in (select distinct monument_id from photos where date_trunc('month',created) = (select date_trunc('month',max(created)) latest_month from photos where monument_id in (select id from monuments where visible)))")
+    @new_monuments = @new_monuments.order("id desc")
     @new_monuments = @new_monuments.page().per(6)
     @index_page = Page.find('index')
   end
   
   def recent
-    @new_monuments = Monument.where("date_trunc('month',created) = (select date_trunc('month',max(created)) from monuments where visible)")
-    @new_monuments = @new_monuments.order("exists (select * from photos where monument_id = monuments.id) desc, id desc")
+    @new_monuments = Monument.where("id in (select distinct monument_id from photos where date_trunc('month',created) = (select date_trunc('month',max(created)) latest_month from photos where monument_id in (select id from monuments where visible)))")
+    @new_monuments = @new_monuments.order("id desc")
     @new_monuments = @new_monuments.page(params[:page]).per(50)
   end
   
