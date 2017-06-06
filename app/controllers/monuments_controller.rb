af74672dc7cff7c46f1b885e6183f85d72914cc2
Jakob Egger <jakob@eggerapps.at>
2017-06-06 22:54:55 +0200

    Fix new monuments section on index page

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -14,9 +14,12 @@ class MonumentsController < ApplicationController
 
   def index
     @query = Query.find(1)
-    @new_monuments = Monument.where("id in (select distinct monument_id from photos where date_trunc('month',created) = (select date_trunc('month',max(created)) latest_month from photos where monument_id in (select id from monuments where visible)))")
+    @new_monuments_date = Photo.select("date_trunc('month',max(created)) AS created").where("monument_id not in (select id from monuments where not visible)")[0].created
+    @new_monuments = Monument.where("id in (select distinct monument_id from photos where date_trunc('month',created) = ?)", @new_monuments_date)
+    @new_monuments_count = @new_monuments.count
     @new_monuments = @new_monuments.order("id desc")
-    @new_monuments = @new_monuments.page().per(6)
+    @new_monuments = @new_monuments.limit(6)
+    @new_monuments = @new_monuments
     @index_page = Page.find('index')
   end
   
