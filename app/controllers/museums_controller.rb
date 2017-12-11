305a57703df5b0b0451d6078d47d05d0b391d1a6
Jakob <jakob@eggerapps.at>
2017-12-11 10:26:11 +0100

    Added Museums page

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -4,5 +4,10 @@ class MuseumsController < ApplicationController
     @museum = Museum.find(params[:id])
     @monuments = @museum.monuments.page(params[:page]).per(100)
   end
+  
+  def index
+    @museums_by_country = Hash.new { |h, k| h[k]=[] }
+    Museum.find_by_sql("SELECT * FROM web_queries.museums_index").each { |museum| @museums_by_country[museum.country_name].push museum }
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def nl2br(str)
     html_escape(str).gsub(/\n/,"<br>\n").html_safe
   end
+  
+  def unumlaut(str)
+    replacements = {
+      "ä"=>"ae",
+      "Ä"=>"Ae",
+      "Ü"=>"Ue",
+      "Ö"=>"Oe",
+      "ö"=>"oe",
+      "ü"=>"ue",
+      "ß"=>"ss",
+      "é"=>"e"
+    }
+    str.gsub(Regexp.union(replacements.keys),replacements).gsub(/\W+/,"_")
+  end
 end
