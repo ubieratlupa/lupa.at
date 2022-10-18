8d1a6d3ee47ba2b996db06617b643e032fd46a5d
Jakob Egger <jakob@eggerapps.at>
2022-10-18 15:31:26 +0200

    Upgrade to Rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -2,22 +2,29 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_131849) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_122438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "dating_phase", ["Antoninisch", "Antoninisch-Severisch", "Augusteisch", "Claudisch", "Constantinisch", "Domitianisch", "Flavisch", "Frühchristlich", "Frühmittelalterlich", "Gallienisch", "Griechisch-Spätklassisch", "Hadrianisch", "Hellenistisch", "Iulisch-Claudisch", "Iustinianisch", "Neronisch", "Prähistorisch", "Republikanisch", "Severisch", "Severisch-Soldatenkaiser", "Soldatenkaiser", "Spätantik", "Spätrepublikanisch", "Tetrarchisch", "Tiberisch", "Traianisch", "Traianisch-Hadrianisch", "Traianisch-Soldatenkaiser", "Vespasianisch", "Vorrömisch", "Griechische Klassik", "Archaik", "Griechische Archaik"]
+  create_enum "inscription_type", ["Bauinschrift", "Besitzerinschrift", "Defixio", "Ehreninschrift", "Gesetz / Juridischer Text", "Grabinschrift", "Graffito", "Grenzstein", "Hospitiumvertrag", "Meilensteininschrift", "Militärdiplom", "Patronatsvertrag", "Sitzinschrift", "Stempel", "Votiv / Kultinschrift", "Einweihung", "Hermeninschrift", "(?)", "Kommemorativinschrift", "mittelalterlich", "neuzeitlich", "Stifterinschrift"]
+  create_enum "material", ["Stein", "Alabaster", "Andesit", "Basalt", "Granit", "Kalkstein", "Konglomerat", "Marmor", "Porphyr", "Sandstein", "Schmuckstein", "Trachit", "Tuffstein", "Mosaik", "Wandmalerei", "Blei", "Bronze", "Eisen", "Holz", "Ton", "Lavez"]
+  create_enum "monument_type", ["(?)", "Architektur", "Brunnen", "Brücke", "Büste oder Statue", "Felsengrab", "Forum", "Grabaedicula", "Grabaedicula (?)", "Grabaedicula (?) Grabbezirk (?)", "Grabaedicula (?) Tumulus (?)", "Grabbau", "Grabbezirk", "Grabbezirk (?)", "Grabkammer", "Heiligtum / Tempel", "Jupiter-Säule", "Kirche", "Militärbau", "Mobiliar", "Nymphaeum", "Palastanlage", "Pfeilergrabmal", "Pfeilergrabmal (?)", "Propylon", "Stadttor / Bogen", "Statue (?)", "Statue / Statuette", "Statuenbasis", "Steinbruch / Bergwerk", "Theater / Amphitheater", "Triumphbogen / Ehrenbogen", "Tropaion / Tropaeum", "Tumulus", "Tumulus (?)"]
+  create_enum "object_type", ["(?)", "Akroter", "Altar", "Altar / Basis", "Altarbekrönung", "Antefix", "Architrav", "Arkade", "Aschenkiste / Urne", "Bank", "Basis ", "Bauquader", "Bleibarren", "Brunnenfassung", "Büste", "Cippus", "Ciste", "Dach", "Decke", "Dromos-Verkleidung", "Eckblock", "Felsinschrift / Felszeichnung / Felsrelief", "Firstbalken", "Fries", "Gefäß", "Gesims", "Gewicht / Hohlmaß", "Giebel", "Grabaltar ", "Grabbau", "Grabeinfassung", "Grabmedaillon", "Grabpfeiler", "Grabstein", "Herme", "Horologion", "Inschrift", "Inschrift zwischen Seitenfeldern", "Jupiter-Säule", "Kameo / Gemme", "Kapitell", "Konsole", "Kopf", "Körpervotiv", "Lorica", "Löwe", "Löwenaufsatz", "Maske", "Meilenstein", "Militärdiplom", "Mosaik", "Oscillum", "Pfeiler", "Pilaster", "Pinienzapfen", "Platte", "Platte mit Ritzzeichnung", "Porträtnische", "Porträtnische mit Inschrift", "Pyramidenstumpf", "Pyramidenstumpf mit Löwen", "Relief", "rundplastische Tierskulptur", "Sarkophag", "Seitenwange einer Grabaedicula", "Sonnenuhr", "Sphinx", "Statue", "Statuenbasis", "Statuette", "Statuette mit Votivinschrift", "Stele", "Säule", "Säulenbasis", "Titulus", "Tondo", "Tropaion (Tropaeum)", "Volute", "Votivaedicula", "Votivaltar", "Votivrelief ", "Wand eines Grabbaus", "Wand mit Pilastern/Halbsäulen", "Wandmalerei", "Wasserspeier", "Ziegel", "Felsrelief/Felsinschrift", "gerahmtes Schriftfeld", "Mobiliar"]
+
   create_table "ancient_places", id: :integer, default: -> { "next_id('vocabulary'::text, 'ancient_places'::text)" }, force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.integer "parent_id"
     t.float "long"
     t.float "lat"
@@ -25,12 +32,13 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
   end
 
   create_table "authors", id: :integer, default: -> { "next_id('vocabulary'::text, 'authors'::text)" }, force: :cascade do |t|
-    t.string "first_name", limit: 50
-    t.string "last_name", limit: 50
+    t.string "first_name"
+    t.string "last_name"
     t.text "institution"
     t.text "comment"
     t.text "email"
     t.boolean "visible", default: true, null: false
+    t.text "credit_name", comment: "Dieser Name erscheint am Datenblatt und bei den Fotos (falls leer, wird first_name + last_name angezeigt)"
   end
 
   create_table "collections", force: :cascade do |t|
@@ -49,16 +57,19 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
     t.text "name"
     t.text "email"
     t.text "comment"
-    t.datetime "created"
+    t.datetime "created", precision: nil
   end
 
   create_table "copyrights", id: :integer, default: -> { "next_id('vocabulary'::text, 'copyrights'::text)" }, force: :cascade do |t|
-    t.string "copyright", limit: 255
+    t.string "copyright"
     t.boolean "publication_permission_required", default: true, null: false
   end
 
-# Could not dump table "dating_phases_time_span" because of following StandardError
-#   Unknown type 'dating_phase' for column 'phase_name'
+  create_table "dating_phases_time_span", id: false, force: :cascade do |t|
+    t.enum "phase_name", enum_type: "dating_phase"
+    t.integer "phase_from"
+    t.integer "phase_to"
+  end
 
   create_table "glossary", id: :serial, force: :cascade do |t|
     t.text "name"
@@ -77,8 +88,64 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
     t.index ["short"], name: "literature_short_key", unique: true
   end
 
-# Could not dump table "monuments" because of following StandardError
-#   Unknown type 'object_type' for column 'object_type'
+  create_table "monuments", id: :integer, default: -> { "next_monument_id()" }, force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "catalog_text"
+    t.string "comment", limit: 255
+    t.enum "object_type", enum_type: "object_type"
+    t.enum "monument_type", enum_type: "monument_type"
+    t.integer "finding_place_id"
+    t.string "finding_place_comment"
+    t.text "finding_comment"
+    t.integer "ancient_finding_place_id"
+    t.integer "conservation_place_id"
+    t.string "conservation_place_comment"
+    t.string "conservation_comment", limit: 255
+    t.string "conservation_state", limit: 255
+    t.integer "regional_info_id"
+    t.integer "museum_id"
+    t.string "museum_inventory_number", limit: 100
+    t.text "iconography"
+    t.enum "inscription_type", enum_type: "inscription_type"
+    t.text "inscription"
+    t.text "inscription_comment"
+    t.text "inscription_variants"
+    t.string "inscription_letter_size", limit: 100
+    t.text "inscription_paleography"
+    t.text "inscription_translation"
+    t.string "inscription_name_donor", limit: 255
+    t.string "inscription_function", limit: 255
+    t.string "inscription_formula", limit: 255
+    t.enum "dating_phase", enum_type: "dating_phase"
+    t.integer "dating_from"
+    t.integer "dating_to"
+    t.string "dating_comment", limit: 255
+    t.integer "parent_monument_id"
+    t.string "width", limit: 20
+    t.string "height", limit: 20
+    t.string "depth", limit: 20
+    t.text "literature"
+    t.text "literature_online"
+    t.enum "material", enum_type: "material"
+    t.text "material_comment"
+    t.string "material_sri", limit: 255
+    t.text "material_local_name"
+    t.text "material_provenance"
+    t.text "material_arguments"
+    t.string "material_analysis_author", limit: 255
+    t.integer "archaeology_author_id"
+    t.integer "epigraphy_author_id"
+    t.integer "architecture_author_id"
+    t.datetime "created", precision: nil, default: -> { "now()" }
+    t.datetime "modified", precision: nil
+    t.boolean "visible", default: false, null: false
+    t.text "column52"
+    t.bigint "collection_id"
+    t.index ["collection_id"], name: "index_monuments_on_collection_id"
+    t.index ["conservation_place_id"], name: "monuments_conservation_place_id_idx"
+    t.index ["finding_place_id"], name: "monuments_finding_place_id_idx"
+    t.index ["modified", "id"], name: "monuments_reverse_update_idx", order: { modified: "DESC NULLS LAST" }
+  end
 
   create_table "museums", id: :integer, default: -> { "next_id('vocabulary'::text, 'museums'::text)" }, force: :cascade do |t|
     t.string "name", limit: 100
@@ -87,6 +154,12 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
     t.text "description"
     t.text "website"
     t.index ["short_name"], name: "museums_short_name_key", unique: true
+    t.check_constraint "short_name ~ '/^[-\\w_]+$/'::text", name: "Numbers, Letters, Underscore, Dash"
+    t.check_constraint "website ~ '^https?://'::text", name: "Begins with http"
+  end
+
+  create_table "neuzugänge", id: false, force: :cascade do |t|
+    t.date "start_date"
   end
 
   create_table "pages", id: :text, force: :cascade do |t|
@@ -110,8 +183,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
     t.integer "ord"
     t.integer "width"
     t.integer "height"
-    t.datetime "created", default: -> { "now()" }
-    t.datetime "modified"
+    t.datetime "created", precision: nil, default: -> { "now()" }
+    t.datetime "modified", precision: nil
     t.index ["monument_id"], name: "photos_monument_id_idx"
   end
 
@@ -124,12 +197,26 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
     t.decimal "long"
     t.decimal "lat"
     t.text "full_name"
-    t.datetime "created"
-    t.datetime "modified"
+    t.datetime "created", precision: nil
+    t.datetime "modified", precision: nil
+  end
+
+  create_table "publications", id: :serial, force: :cascade do |t|
+    t.text "title"
+    t.text "authors"
+    t.integer "year"
+    t.text "citation"
+    t.text "abstract"
+    t.text "link"
+    t.text "doi"
+    t.text "pdf_link"
+    t.text "monument_ids"
+    t.timestamptz "created", default: -> { "now()" }
+    t.timestamptz "modified"
   end
 
   create_table "queries", id: :integer, default: -> { "((('x'::text || encode(gen_random_bytes(4), 'hex'::text)))::bit(31))::integer" }, force: :cascade do |t|
-    t.datetime "created", default: -> { "now()" }, null: false
+    t.timestamptz "created", default: -> { "now()" }, null: false
     t.text "keywords"
     t.text "finding_place_legacy"
     t.text "conservation_place_legacy"
@@ -147,6 +234,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
     t.integer "museum_id"
     t.integer "dating_from"
     t.integer "dating_to"
+    t.text "object_type"
+    t.enum "inscription_type", enum_type: "inscription_type"
   end
 
   create_table "regional_info_links", id: :integer, default: -> { "next_id('vocabulary'::text, 'regional_info_links'::text)" }, force: :cascade do |t|
@@ -163,8 +252,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
     t.text "opening_hours"
     t.string "phone", limit: 255
     t.string "email", limit: 255
-    t.datetime "created", default: -> { "now()" }
-    t.datetime "modified"
+    t.datetime "created", precision: nil, default: -> { "now()" }
+    t.datetime "modified", precision: nil
   end
 
   add_foreign_key "ancient_places", "ancient_places", column: "parent_id", name: "ancient_places_parent_id_fkey"
@@ -175,7 +264,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_131849) do
   add_foreign_key "monuments", "authors", column: "architecture_author_id", name: "monuments_architecture_author_id_fkey", on_update: :cascade
   add_foreign_key "monuments", "authors", column: "epigraphy_author_id", name: "monuments_epigraphy_author_id_fkey", on_update: :cascade
   add_foreign_key "monuments", "collections"
-  add_foreign_key "monuments", "monuments", column: "parent_monument_id", name: "monuments_related_monument_id_fkey"
+  add_foreign_key "monuments", "monuments", column: "parent_monument_id", name: "monuments_related_monument_id_fkey", on_update: :cascade
   add_foreign_key "monuments", "museums", name: "monuments_museum_id_fkey"
   add_foreign_key "monuments", "places", column: "conservation_place_id", name: "monuments_conservation_place_id_fkey"
   add_foreign_key "monuments", "places", column: "finding_place_id", name: "monuments_finding_place_id_fkey"
