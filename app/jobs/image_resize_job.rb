80cb0290cb5cb7de79f6513c8c27a32e277c42c9
Jakob Egger <jakob@eggerapps.at>
2026-03-20 13:33:45 +0100

    Fix resize bug

--- app/jobs/image_resize_job.rb
+++ app/jobs/image_resize_job.rb
@@ -9,7 +9,7 @@ class ImageResizeJob < ApplicationJob
     download_url = photo.download_url
     filename = photo.filename
     
-    stdout, stderr, status = Open3.capture3("curl -L #{Shellwords.escape(download_url)} | gm convert -size 2000x2000 - -resize '2000x2000>' +profile '*' /var/www/upload/image/#{Shellwords.escape(filename)}")
+    stdout, stderr, status = Open3.capture3("curl -L #{Shellwords.escape(download_url)} | gm convert -size 2000x2000 - -resize '2000x2000>' +profile '*' /var/www/upload/img/#{Shellwords.escape(filename)}")
 
     Rails.logger.info "Image Resize Command Output: #{stdout}" unless stdout.empty?
 
@@ -19,7 +19,7 @@ class ImageResizeJob < ApplicationJob
 
     unless status.success?
       Rails.logger.error "Image Resize Command failed with exit code #{status.exitstatus}"
-      raise "Image Resize Command failed"
+      raise "Image Resize failed for #{photo.basename}"
     end
     
   end
