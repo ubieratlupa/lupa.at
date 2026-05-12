81ac18b539946590fb8d6cc8e87b0827d7544234
Jakob Egger <jakob@eggerapps.at>
2026-05-12 10:35:14 +0200

    Update cache version to fix rails startup warning

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module LupaAt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
+    
+    config.active_support.cache_format_version = 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
